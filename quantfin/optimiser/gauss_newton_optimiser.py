39437a8292595f4867b8b283c29202c96a1e93ed
ilham-raslan <ilhamraslan@yahoo.com>
2025-12-07 14:44:32 +0000

    WIP GN Optimiser

---
+++ quantfin/optimiser/gauss_newton_optimiser.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+class GaussNewtonOptimiser:
+    def __init__(self, residuals, expiries, strikes, market_vols, forwards):
+        self.residuals = residuals
+        self.expiries = expiries
+        self.strikes = strikes
+        self.market_vols = market_vols
+        self.forwards = forwards
+
+    def jacobian(self, params, expiries, strikes, forwards, market_vols):
+        # Finite difference calculation of the jacobians
+        x = params
+        base_residuals = self.residuals(x, self.expiries, self.strikes, self.forwards, self.market_vols)
+        # bump alpha up by 0.01
+        alpha_up_x = x[0] + 0.01, x[1], x[2]
+        alpha_up_residuals = self.residuals(alpha_up_x, self.expiries, self.strikes, self.forwards, self.market_vols)
+
+        rho_up_x = x[0], x[1] + 0.01, x[2]
+        rho_up_residuals = self.residuals(rho_up_x, self.expiries, self.strikes, self.forwards, self.market_vols)
+
+        nu_up_x = x[0], x[1], x[2] + 0.01
+        nu_up_residuals = self.residuals(nu_up_x, self.expiries, self.strikes, self.forwards, self.market_vols)
+
+        data_points = len(self.expiries)
+        alpha_deltas = (alpha_up_residuals - base_residuals) / 0.01
+        rho_deltas = (rho_up_residuals - base_residuals) / 0.01
+        nu_deltas = (nu_up_residuals - base_residuals) / 0.01
+
+        jacobian = np.hstack((alpha_deltas.reshape(data_points, 1), rho_deltas.reshape(data_points, 1), nu_deltas).reshape(data_points, 1))
+
+        print("Dummy")
+
+        x1, x2 = x
+        return np.array([
+            [ 2 * x1, 1 ],
+            [ 1, 2 * x2 ]
+        ])
+
+    def optimise(self, x0, max_iter=20, tol=1e-6):
+        x = x0[0], x0[1], x0[2]
+
+        residuals = self.residuals(x, self.expiries, self.strikes, self.forwards, self.market_vols)
+        jacobian = self.jacobian(x, self.expiries, self.strikes, self.forwards, self.market_vols)
+
+        print("Dummy")
+
+        # for k in range(max_iter):
+        #     r = self.residuals(x)
+        #     print(f"Iter {k}: ||r|| = {np.linalg.norm(r):.4e}, x = {x}")
+        #
+        #     J = jacobian(x)
+        #
+        #     p = -np.linalg.inv(J.T @ J) @ (J.T @ r)
+        #
+        #     x_new = x + p
+        #     r_new = residuals(x)
+        #
+        #     print(f"Iter {k}: ||r|| = {np.linalg.norm(r_new):.4e}, x = {x_new},")
+        #     x = x_new
+        #
+        #     if np.linalg.norm(p) < tol:
+        #         break
+
+        return x
--- quantfin/vol/vol_calibrator.py
+++ quantfin/vol/vol_calibrator.py
@@ -1,17 +1,20 @@
 import numpy as np
 from scipy.optimize import least_squares
 
+from quantfin.optimiser.gauss_newton_optimiser import GaussNewtonOptimiser
 from quantfin.vol.vol_model import VolModel
 
 
 class VolCalibrator:
     """Generic VolCalibrator class"""
 
-    def __init__(self, expiries, strikes, market_vols, forwards):
+    # def __init__(self, expiries, strikes, market_vols, forwards, engine="scipy"):
+    def __init__(self, expiries, strikes, market_vols, forwards, engine="gauss_newton_unconstrained"):
         self.expiries = expiries
         self.strikes = strikes
         self.market_vols = market_vols
         self.forwards = forwards
+        self.engine = engine
 
     def residuals(self, params, expiries, strikes, forwards, market_vols):
         alpha, rho, nu = params
@@ -23,37 +26,46 @@ class VolCalibrator:
         ])
 
     def calibrate(self):
-        expiries = np.array(self.expiries)
-        strikes = np.array(self.strikes)
-        forwards = np.array(self.forwards)
-        market_vols = np.array(self.market_vols)
+        if self.engine == "scipy":
+            expiries = np.array(self.expiries)
+            strikes = np.array(self.strikes)
+            forwards = np.array(self.forwards)
+            market_vols = np.array(self.market_vols)
 
-        # initial guesses
-        x0 = np.array([0.2, -0.2, 0.5])
+            # initial guesses
+            x0 = np.array([0.2, -0.2, 0.5])
 
-        lower = [1e-8, -0.999, 1e-8]
-        upper = [10.0, 0.999, 5.0]
+            lower = [1e-8, -0.999, 1e-8]
+            upper = [10.0, 0.999, 5.0]
 
-        result = least_squares(
-            fun=self.residuals,
-            x0=x0,
-            bounds=(lower, upper),
-            args=(expiries, strikes, forwards, market_vols),
-            method='trf'
-        )
+            result = least_squares(
+                fun=self.residuals,
+                x0=x0,
+                bounds=(lower, upper),
+                args=(expiries, strikes, forwards, market_vols),
+                method='trf'
+            )
 
-        residuals = result.fun
+            residuals = result.fun
 
-        # RMS error
-        rms_error = np.sqrt(np.mean(residuals ** 2))
-        print("RMS error:", rms_error)
+            # RMS error
+            rms_error = np.sqrt(np.mean(residuals ** 2))
+            print("RMS error:", rms_error)
 
-        # Maximum absolute residual
-        max_error = np.max(np.abs(residuals))
-        print("Max absolute residual:", max_error)
+            # Maximum absolute residual
+            max_error = np.max(np.abs(residuals))
+            print("Max absolute residual:", max_error)
 
-        # Fitted parameters
-        alpha_fit, rho_fit, nu_fit = result.x
-        print("Fitted params:", alpha_fit, rho_fit, nu_fit)
+            # Fitted parameters
+            alpha_fit, rho_fit, nu_fit = result.x
+            print("Fitted params:", alpha_fit, rho_fit, nu_fit)
+        elif self.engine == "gauss_newton_unconstrained":
+            optimiser = GaussNewtonOptimiser(self.residuals, self.expiries, self.strikes, self.market_vols, self.forwards)
+            x0 = np.array([0.2, -0.2, 0.5])
+            params = optimiser.optimise(x0)
+        elif self.engine == "levenberg_marquardt_unconstrained":
+            pass
+        else:
+            raise Exception("Calibration engine " + self.engine + " unsupported")
 
         return VolModel(alpha_fit, rho_fit, nu_fit)
