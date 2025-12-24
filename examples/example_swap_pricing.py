9da939b5d55ce213a9937bfd5bf5c60cc2a30d29
ilham-raslan <ilhamraslan@yahoo.com>
2025-12-24 09:49:48 +0000

    Attempt at linear interpolation between 0 and 0.25

--- examples/example_swap_pricing.py
+++ examples/example_swap_pricing.py
@@ -8,9 +8,9 @@ ois_swaps = OIS_SWAPS
 swaps3m = SWAPS_3M
 
 curve_manager = CurveManager()
-curves = curve_manager.build(ois_swaps, swaps3m, model="log_linear_bootstrapped")
+# curves = curve_manager.build(ois_swaps, swaps3m, model="log_linear_bootstrapped")
 # still working on nelson siegel optimisation
-# curves = curve_manager.build(ois_swaps, swaps3m, mode="nelson_siegel", calibration_engine="levenberg_marquardt")
+curves = curve_manager.build(ois_swaps, swaps3m, model="nelson_siegel", calibration_engine="levenberg_marquardt")
 
 ois_curve = curves["ois"]
 ibor3m_curve = curves["3m"]
--- quantfin/curves/nelson_siegel_curve_model.py
+++ quantfin/curves/nelson_siegel_curve_model.py
@@ -9,8 +9,20 @@ class NelsonSiegelCurveModel:
         self.tau = tau
 
     def df(self, t):
-        power_term = self.beta0 * t + (self.beta1 + self.beta2) * (1 - math.exp(-t / self.tau) * self.tau) - self.beta2 * t * math.exp(-t / self.tau)
-        return math.exp(-power_term)
+        # Linear interpolation between 0 and 0.25
+        if t <= 0.25:
+            t_knot = 0.25
+            power_term = self.beta0 * t_knot + (self.beta1 + self.beta2) * (1 - math.exp(-t_knot / self.tau) * self.tau) - self.beta2 * t_knot * math.exp(-t_knot / self.tau)
+            df_knot = math.exp(-power_term)
+            df_0 = 1
+
+            df = df_0 + (t / t_knot) * (df_knot - df_0)
+        else:
+            power_term = self.beta0 * t + (self.beta1 + self.beta2) * (1 - math.exp(-t / self.tau) * self.tau) - self.beta2 * t * math.exp(-t / self.tau)
+
+            df = math.exp(-power_term)
+
+        return df
 
     def zero_rate(self, t):
         return -np.log(self.df(t) / t)
